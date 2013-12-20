9ffca329a9d588aeff0c3cd663af1bdedef67089
Dale Stammen <dalestam@microsoft.com>
2013-12-20 06:38:37 -0800

    added egl platform defines for WinRT and Windows Phone 8.0
    Change-Id: I466689fd87b45e41aac3e1d1c99d55cacddfbd31

--- include/EGL/eglplatform.h
+++ include/EGL/eglplatform.h
@@ -73,9 +73,31 @@
 #endif
 #include <windows.h>
 
+#if defined(WINAPI_FAMILY)
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) && !defined(ANGLE_PLATFORM_WINRT)
+
 typedef HDC     EGLNativeDisplayType;
 typedef HBITMAP EGLNativePixmapType;
 typedef HWND    EGLNativeWindowType;
+
+#else
+
+#include <wrl\client.h>
+#define WINRT_EGL_IUNKNOWN(x) reinterpret_cast<IUnknown *>(x)
+typedef Microsoft::WRL::ComPtr<IUnknown> EGLNativeWindowType;
+typedef EGLNativeWindowType EGLNativeDisplayType;
+typedef HBITMAP EGLNativePixmapType;
+
+#endif
+
+#elif defined(_WIN32) || defined(_WIN64)
+
+typedef HDC     EGLNativeDisplayType;
+typedef HBITMAP EGLNativePixmapType;
+typedef HWND    EGLNativeWindowType;
+
+#endif
 
 #elif defined(__WINSCW__) || defined(__SYMBIAN32__)  /* Symbian */
 
